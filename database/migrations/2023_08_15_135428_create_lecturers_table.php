71cebdadfc21448a4dc735a9cb552ef338ef2b72
Ashisoma <okalionesmus@gmail.com>
2023-08-15 17:10:01 +0300

    initial commit

---
+++ database/migrations/2023_08_15_135428_create_lecturers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lecturers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('course_id');
+            $table->dateTime('date_time');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            // Add more columns as needed
+            
+            $table->timestamps();
+
+            $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lecturers');
+    }
+};
